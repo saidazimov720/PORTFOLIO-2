968e75b5b3b77935785e7a62cf42c18cbe1089e9
saidazimov720 <saidazimovnurbek01gmail.com>
2024-04-20 13:30:00 +0500

    trigger

--- js/script.js
+++ js/script.js
@@ -8,4 +8,14 @@ if (ScrollTrigger.isTouch !==1) {
 		smooth: 1.5,
 		effects: true
 	})
+
+    gsap.fromTo('.hero-section', {opacity: 1}, {
+        opacity: 0,
+        scrollTrigger: {
+            trigger: '.hero-section',
+			start: 'center',
+			end: '820',
+			scrub: true
+        }
+    })
 }
